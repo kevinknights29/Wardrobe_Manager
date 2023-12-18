a613d155769134fd25603eb64db8b50b0b260aef
Kevin Knights <kevinknights29@hotmail.com>
2023-12-17 22:43:58 -0500

    feat: added utils module

---
+++ src/utils/constants.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+import os
+
+
+# Directories
+UTILS_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_PACKAGE = os.path.dirname(UTILS_DIR)
+ROOT = os.path.dirname(PROJECT_PACKAGE)
+
+# Files
+CONFIG_FILE = os.path.join(ROOT, "config.yaml")
+
+# Logging
+LOGGING_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
